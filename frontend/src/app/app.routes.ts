3ae7c6f7d7556e58d502bfd83b7dc2f3adc602d2
drr1201 <drr1201@p2es.com>
2026-08-13 21:19:56 +0530

    Added the authorization parts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
@@ -9,6 +10,7 @@ export const routes: Routes = [
   },
   {
     path: 'tasks',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./features/tasks/task-page/task-page').then((m) => m.TaskPage),
   },
--- frontend/src/app/core/auth.service.ts
+++ frontend/src/app/core/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export interface UserDto {
   id: number;
@@ -8,14 +9,18 @@ export interface UserDto {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private http = inject(HttpClient);
-  private api = 'http://localhost:5206/api/auth';
-
-  login(username: string, password: string) {
-    return this.http.post<UserDto>(
-      `${this.api}/login`,
-      { username, password },
-      { withCredentials: true }
-    );
+    private http = inject(HttpClient);
+    private api = 'http://localhost:5206/api/auth';
+  
+    isLoggedIn = false;
+  
+    login(username: string, password: string) {
+      return this.http.post<UserDto>(
+        `${this.api}/login`,
+        { username, password },
+        { withCredentials: true }
+      ).pipe(
+        tap(() => (this.isLoggedIn = true))
+      );
+    }
   }
-}
